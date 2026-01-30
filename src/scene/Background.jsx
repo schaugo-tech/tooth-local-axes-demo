d00ca0c3908e18dfced12a2594dfff736a510a12
Schaugo <zhou@schaugo.com>
2026-01-30 17:21:42 +0800

    "fix: background shader string safe for PS"

--- src/scene/Background.jsx
+++ src/scene/Background.jsx
@@ -11,24 +11,22 @@ export default function Background() {
         topColor: { value: new THREE.Color('#f7f9ff') },
         bottomColor: { value: new THREE.Color('#eef2fb') }
       },
-      vertexShader: 
-        varying vec3 vWorldPosition;
-        void main() {
-          vec4 worldPosition = modelMatrix * vec4(position, 1.0);
-          vWorldPosition = worldPosition.xyz;
-          gl_Position = projectionMatrix * viewMatrix * worldPosition;
-        }
-      ,
-      fragmentShader: 
-        uniform vec3 topColor;
-        uniform vec3 bottomColor;
-        varying vec3 vWorldPosition;
-        void main() {
-          float h = normalize(vWorldPosition).y * 0.5 + 0.5;
-          vec3 col = mix(bottomColor, topColor, h);
-          gl_FragColor = vec4(col, 1.0);
-        }
-      
+      vertexShader:
+        'varying vec3 vWorldPosition;\n' +
+        'void main() {\n' +
+        '  vec4 worldPosition = modelMatrix * vec4(position, 1.0);\n' +
+        '  vWorldPosition = worldPosition.xyz;\n' +
+        '  gl_Position = projectionMatrix * viewMatrix * worldPosition;\n' +
+        '}\n',
+      fragmentShader:
+        'uniform vec3 topColor;\n' +
+        'uniform vec3 bottomColor;\n' +
+        'varying vec3 vWorldPosition;\n' +
+        'void main() {\n' +
+        '  float h = normalize(vWorldPosition).y * 0.5 + 0.5;\n' +
+        '  vec3 col = mix(bottomColor, topColor, h);\n' +
+        '  gl_FragColor = vec4(col, 1.0);\n' +
+        '}\n'
     })
   }, [])
 
